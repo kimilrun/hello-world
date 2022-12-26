41bdcfc8b9732a91befef2dec34bc280ee55724e
ykkim-demon <ykkim76@gmail.com>
2022-12-27 08:27:03 +0900

    identity basic

---
+++ hello-world-identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hello_world_identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
